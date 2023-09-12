b55385190ae1ef3862262abf90c14b02494e3b60
ylides <e.propagating@gmail.com>
2023-09-12 15:46:24 +0900

    add packages

---
+++ sourcemap/src/lib.rs
@@ -0,0 +1,49 @@
+#[derive(Debug, Clone)]
+pub struct Loc<Size = usize> {
+    pub char_pos: Size,
+}
+
+impl<Size> Loc<Size> {
+    pub fn new(char_pos: Size) -> Self {
+        Self { char_pos }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Span<T> {
+    pub start: T,
+    pub end: T,
+}
+
+impl<U> Span<Loc<U>> {
+    pub fn range(&self) -> std::ops::Range<U>
+    where
+        U: std::ops::Add + Copy,
+    {
+        self.start.char_pos..self.end.char_pos
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Spanned<T, U = Loc<usize>> {
+    pub node: T,
+    pub span: Span<U>,
+}
+
+impl<T, U> Spanned<T, U> {
+    pub fn new(node: T, (start, end): (U, U)) -> Self {
+        Self {
+            node,
+            span: Span { start, end },
+        }
+    }
+}
+
+impl<T, U> Spanned<T, Loc<U>> {
+    pub fn range(&self) -> std::ops::Range<U>
+    where
+        U: std::ops::Add + Copy,
+    {
+        self.span.range()
+    }
+}
---
+++ syntax/src/lib.rs
@@ -0,0 +1,110 @@
+use std::fmt::Display;
+
+use sourcemap::Spanned;
+
+pub type Ident<'input> = &'input str;
+
+pub type Expr<'a> = Spanned<ExprKind<'a>>;
+
+#[derive(Debug, Clone)]
+pub enum ExprKind<'input> {
+    Lit(LitKind),
+    Unary(UnOp, Box<Expr<'input>>),
+    Binary(BinOp, Box<Expr<'input>>, Box<Expr<'input>>),
+    If(Box<Expr<'input>>, Box<Expr<'input>>, Box<Expr<'input>>),
+    Let(LetKind<'input>),
+    Then(Box<Expr<'input>>, Box<Expr<'input>>),
+    Var(Ident<'input>),
+    App(Box<Expr<'input>>, Vec<Expr<'input>>),
+    Tuple(Vec<Expr<'input>>),
+    ArrayMake(Box<Expr<'input>>, Box<Expr<'input>>),
+    Get(Box<Expr<'input>>, Box<Expr<'input>>),
+    Set(Box<Expr<'input>>, Box<Expr<'input>>, Box<Expr<'input>>),
+}
+
+#[derive(Debug, Clone)]
+pub struct FunDef<'a> {
+    pub name: Ident<'a>,
+    pub args: Vec<Ident<'a>>,
+    pub body: Box<Expr<'a>>,
+}
+
+impl<'a> FunDef<'a> {
+    pub fn new(name: Ident<'a>, args: Vec<Ident<'a>>, body: Box<Expr<'a>>) -> Self {
+        Self { name, args, body }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum BBinOpKind {
+    Eq,
+    Le,
+    Ge,
+    Ne,
+    Lt,
+    Gt,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum IBinOpKind {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Sll,
+    Srl,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum FBinOpKind {
+    FAdd,
+    FSub,
+    FMul,
+    FDiv,
+    Fless,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum FUnOpKind {
+    Fneg,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum BinOp {
+    BBinOp(BBinOpKind),
+    IBinOp(IBinOpKind),
+    FBinOp(FBinOpKind),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum UnOp {
+    Not,
+    Neg,
+    FNeg,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum LitKind {
+    Unit,
+    Bool(bool),
+    Int(i32),
+    Float(f32),
+}
+
+impl Display for LitKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LitKind::Unit => write!(f, "()"),
+            LitKind::Bool(b) => write!(f, "{b}"),
+            LitKind::Int(i) => write!(f, "{i}"),
+            LitKind::Float(fl) => write!(f, "{fl}"),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum LetKind<'a> {
+    LetVar(Ident<'a>, Box<Expr<'a>>, Box<Expr<'a>>),
+    LetRec(FunDef<'a>, Box<Expr<'a>>),
+    LetTuple(Vec<Ident<'a>>, Box<Expr<'a>>, Box<Expr<'a>>),
+}
