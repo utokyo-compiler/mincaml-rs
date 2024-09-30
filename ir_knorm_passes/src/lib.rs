15e60ca24128382e7e1495880409bfc16ad3d403
azaika <takaaza@gmail.com>
2024-09-30 15:54:09 +0900

    implement let-flattening

--- ir_knorm_passes/src/lib.rs
+++ ir_knorm_passes/src/lib.rs
@@ -2,6 +2,7 @@ use ir_knorm::Expr;
 use middleware::GlobalContext;
 
 mod beta_convert;
+mod let_flatten;
 
 pub trait KnormPass<'ctx> {
     fn run_pass(&mut self, ctx: &'ctx GlobalContext<'ctx>, expr: &mut Expr<'ctx>);
