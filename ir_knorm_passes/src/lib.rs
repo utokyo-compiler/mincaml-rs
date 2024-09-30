892807bd0c63590c10a890a6cb5513a665e6e3c0
azaika <takaaza@gmail.com>
2024-09-30 19:27:45 +0900

    Implement alpha renaming

--- ir_knorm_passes/src/lib.rs
+++ ir_knorm_passes/src/lib.rs
@@ -3,6 +3,8 @@ use middleware::GlobalContext;
 
 mod beta_convert;
 mod let_flatten;
+mod inlining;
+mod alpha_rename;
 
 pub trait KnormPass<'ctx> {
     fn run_pass(&mut self, ctx: &'ctx GlobalContext<'ctx>, expr: &mut Expr<'ctx>);
