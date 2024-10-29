068c0bbdb4353b8c13c911c63e14f8a7f676b21e
s-ylide <71720482+s-ylide@users.noreply.github.com>
2024-10-29 18:14:02 +0900

    support syntax like "0."

--- parser/src/lexer.rs
+++ parser/src/lexer.rs
@@ -27,7 +27,7 @@ pub trait Lexer<'input>: Iterator<Item = Result<'input, Spanned<Token<'input>>>>
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Token<'input> {
     Int(i32),
     Bool(bool),
@@ -78,9 +78,48 @@ pub enum ErrorKind<'input> {
     #[error("unrecognized token `{0}`")]
     UnrecognizedToken(&'input str),
     #[error("cannot parse `{0}` as an integer constant: {1}")]
-    IllegalIntegerConstant(&'input str, <i32 as FromStr>::Err),
+    IllegalIntegerConstant(&'input str, ParseIntegerErr),
     #[error("cannot parse `{0}` as a float constant: {1}")]
     IllegalFloatConstant(&'input str, <f32 as FromStr>::Err),
 }
 
+#[derive(Debug, Clone, Error)]
+pub enum ParseIntegerErr {
+    #[error(transparent)]
+    ParseIntError(std::num::ParseIntError),
+    #[error("leading zeros are not allowed")]
+    LeadingZero,
+}
+
 pub type Result<'input, T> = std::result::Result<T, Error<'input>>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lexer() {
+        let l = sourcemap::Loc::new;
+        let r = SelectedLexer::new("1 + 2 * 3").read_to_vec();
+        assert_eq!(
+            r.unwrap_or_else(|e| panic!("{e:#?}")),
+            vec![
+                Spanned::new(Token::Int(1), (l(0), l(1))),
+                Spanned::new(Token::Plus, (l(2), l(3))),
+                Spanned::new(Token::Int(2), (l(4), l(5))),
+                Spanned::new(Token::Ast, (l(6), l(7))),
+                Spanned::new(Token::Int(3), (l(8), l(9))),
+            ]
+        );
+        let r = SelectedLexer::new("00").read_to_vec();
+        assert!(r.is_err(), "{r:#?}");
+        let r = SelectedLexer::new("0. 2").read_to_vec();
+        assert_eq!(
+            r.unwrap_or_else(|e| panic!("{e:#?}")),
+            vec![
+                Spanned::new(Token::Float(0.), (l(0), l(2))),
+                Spanned::new(Token::Int(2), (l(3), l(4))),
+            ]
+        );
+    }
+}
