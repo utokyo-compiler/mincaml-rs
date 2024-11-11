510dd7c1fa9d78a06a66a4e287fec499694911d6
sylide <71720482+s-ylide@users.noreply.github.com>
2024-11-12 01:46:11 +0900

    move `unify`

--- typing/src/lib.rs
+++ typing/src/lib.rs
@@ -3,10 +3,13 @@
 
 use data_structure::index::vec::IndexVec;
 use sourcemap::Spanned;
-use ty::{context::CommonTypes, Ty, TyKind, TyVarId, Typed};
+use ty::{context::CommonTypes, Ty, TyVarId, Typed};
 
 mod name_res;
 mod ty_var_subst;
+mod unify;
+
+use unify::unify;
 
 pub type Context<'ctx> = ty::context::TypingContext<
     'ctx,
@@ -324,84 +327,4 @@ fn decide_ty_many<'ctx>(
         typed.push(e);
     }
     Ok(typed)
-}
-
-fn unify<'ctx>(
-    subst: &mut ty_var_subst::Env<'ctx>,
-    lhs: Ty<'ctx>,
-    rhs: Ty<'ctx>,
-) -> Result<(), Error<'ctx>> {
-    // N.B., This comparison covers the case
-    // where both `lhs` and `rhs` are the same type variable
-    // so as not to cause an error in `occurck`.
-    if lhs == rhs {
-        return Ok(());
-    }
-    match (lhs.kind(), rhs.kind()) {
-        (TyKind::TyVar(v), _) if let Some(ty) = subst.get(*v) => unify(subst, ty, rhs),
-        (_, TyKind::TyVar(v)) if let Some(ty) = subst.get(*v) => unify(subst, lhs, ty),
-        (TyKind::TyVar(v), _) => {
-            occurck(subst, *v, rhs).unwrap();
-            subst.merge(*v, rhs);
-            Ok(())
-        }
-        (_, TyKind::TyVar(v)) => {
-            occurck(subst, *v, lhs).unwrap();
-            subst.merge(*v, lhs);
-            Ok(())
-        }
-        (TyKind::Fun(lhs_args, lhs_ret), TyKind::Fun(rhs_args, rhs_ret)) => {
-            if lhs_args.len() != rhs_args.len() {
-                return Err(Error::UnifyFailed(lhs, rhs));
-            }
-            for (lhs_arg, rhs_arg) in lhs_args.iter().zip(rhs_args) {
-                unify(subst, *lhs_arg, *rhs_arg).unwrap();
-            }
-            unify(subst, *lhs_ret, *rhs_ret)
-        }
-        (TyKind::Tuple(lhs_tys), TyKind::Tuple(rhs_tys)) => {
-            if lhs_tys.len() != rhs_tys.len() {
-                return Err(Error::UnifyFailed(lhs, rhs));
-            }
-            for (lhs, rhs) in lhs_tys.iter().zip(rhs_tys) {
-                unify(subst, *lhs, *rhs).unwrap();
-            }
-            Ok(())
-        }
-        (TyKind::Array(lhs), TyKind::Array(rhs)) => unify(subst, *lhs, *rhs),
-        _ => Err(Error::UnifyFailed(lhs, rhs)),
-    }
-}
-
-/// Check if the type variable `var` occurs in the type `ty`.
-fn occurck<'ctx>(
-    subst: &ty_var_subst::Env<'ctx>,
-    var: TyVarId,
-    ty: Ty<'ctx>,
-) -> Result<(), Error<'ctx>> {
-    match ty.kind() {
-        TyKind::Fun(args, ret) => {
-            for arg in args {
-                occurck(subst, var, *arg).unwrap();
-            }
-            occurck(subst, var, *ret)
-        }
-        TyKind::Tuple(tys) => {
-            for ty in tys {
-                occurck(subst, var, *ty).unwrap();
-            }
-            Ok(())
-        }
-        TyKind::Array(ty) => occurck(subst, var, *ty),
-        TyKind::TyVar(v) => {
-            if var == *v {
-                Err(Error::OccurckFailed(var, ty))
-            } else if let Some(ty) = subst.get(*v) {
-                occurck(subst, var, ty)
-            } else {
-                Ok(())
-            }
-        }
-        _ => Ok(()),
-    }
 }
