8dfb4b42bf47be0c38f0308668ce909ed2fd9a06
sylide <71720482+s-ylide@users.noreply.github.com>
2024-11-13 05:33:29 +0900

    add display impls

--- data_structure/src/interning.rs
+++ data_structure/src/interning.rs
@@ -1,6 +1,6 @@
 use std::{
     borrow::Borrow,
-    fmt::Debug,
+    fmt::{Debug, Display},
     hash::{Hash, Hasher},
     ops::Deref,
     sync::Mutex,
@@ -77,6 +77,12 @@ impl<T: ?Sized> Deref for Interned<'_, T> {
 
     fn deref(&self) -> &Self::Target {
         self.0
+    }
+}
+
+impl<T: ?Sized + Display> Display for Interned<'_, T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
     }
 }
 
--- syntax/src/prettify.rs
+++ syntax/src/prettify.rs
@@ -1,6 +1,9 @@
 use std::fmt::Display;
 
-use crate::{BinOp, FloatBinOpKind, IntBinOpKind, LitKind, RelationBinOpKind, UnOp};
+use crate::{
+    BinOp, ExprKind, FloatBinOpKind, IntBinOpKind, LetBinder, LitKind, Pattern, RelationBinOpKind,
+    UnOp,
+};
 
 impl RelationBinOpKind {
     pub fn name(self) -> &'static str {
@@ -11,6 +14,19 @@ impl RelationBinOpKind {
             Self::Ne => "ne",
             Self::Lt => "lt",
             Self::Gt => "gt",
+        }
+    }
+}
+
+impl Display for RelationBinOpKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RelationBinOpKind::Eq => write!(f, "="),
+            RelationBinOpKind::Le => write!(f, "<="),
+            RelationBinOpKind::Ge => write!(f, ">="),
+            RelationBinOpKind::Ne => write!(f, "<>"),
+            RelationBinOpKind::Lt => write!(f, "<"),
+            RelationBinOpKind::Gt => write!(f, ">"),
         }
     }
 }
@@ -26,6 +42,17 @@ impl IntBinOpKind {
     }
 }
 
+impl Display for IntBinOpKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            IntBinOpKind::Add => write!(f, "+"),
+            IntBinOpKind::Sub => write!(f, "-"),
+            IntBinOpKind::Mul => write!(f, " * "),
+            IntBinOpKind::Div => write!(f, "/"),
+        }
+    }
+}
+
 impl FloatBinOpKind {
     pub fn name(self) -> &'static str {
         match self {
@@ -33,6 +60,17 @@ impl FloatBinOpKind {
             Self::FSub => "fsub",
             Self::FMul => "fmul",
             Self::FDiv => "fdiv",
+        }
+    }
+}
+
+impl Display for FloatBinOpKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FloatBinOpKind::FAdd => write!(f, "+."),
+            FloatBinOpKind::FSub => write!(f, "-."),
+            FloatBinOpKind::FMul => write!(f, " *. "),
+            FloatBinOpKind::FDiv => write!(f, "/."),
         }
     }
 }
@@ -47,12 +85,32 @@ impl BinOp {
     }
 }
 
+impl Display for BinOp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BinOp::Relation(kind) => write!(f, "{kind}"),
+            BinOp::Int(kind) => write!(f, "{kind}"),
+            BinOp::Float(kind) => write!(f, "{kind}"),
+        }
+    }
+}
+
 impl UnOp {
     pub fn name(self) -> &'static str {
         match self {
             Self::Neg => "neg",
             Self::Not => "not",
             Self::FNeg => "fneg",
+        }
+    }
+}
+
+impl Display for UnOp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            UnOp::Neg => write!(f, "-"),
+            UnOp::Not => write!(f, "not "),
+            UnOp::FNeg => write!(f, "-."),
         }
     }
 }
@@ -74,7 +132,66 @@ impl Display for LitKind {
             LitKind::Unit => write!(f, "()"),
             LitKind::Bool(b) => write!(f, "{b}"),
             LitKind::Int(i) => write!(f, "{i}"),
-            LitKind::Float(f_bits) => write!(f, "{}", f32::from_bits(*f_bits)),
+            LitKind::Float(f_bits) => write!(f, "{:.1}", f32::from_bits(*f_bits)),
+        }
+    }
+}
+
+impl Display for ExprKind<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ExprKind::Const(lit_kind) => write!(f, "{lit_kind}"),
+            ExprKind::Unary(un_op, e) => write!(f, "{un_op}{e}"),
+            ExprKind::Binary(bin_op, e1, e2) => write!(f, "({e1} {bin_op} {e2})"),
+            ExprKind::If(e1, e2, e3) => write!(f, "if {e1} then ({e2}) else ({e3})"),
+            ExprKind::Let(
+                LetBinder {
+                    pattern,
+                    args,
+                    value,
+                },
+                e,
+            ) => {
+                let pattern = match pattern {
+                    Pattern::Var(v) => v.to_string(),
+                    Pattern::Tuple(vs) => vs
+                        .iter()
+                        .map(|v| v.to_string())
+                        .collect::<Vec<_>>()
+                        .join(", "),
+                };
+                if args.is_empty() {
+                    write!(f, "let {pattern} = {value} in\n{e}",)
+                } else {
+                    write!(
+                        f,
+                        "let rec {pattern} {args} = {value} in\n{e}",
+                        args = args.iter().map(|arg| arg.0).collect::<Vec<_>>().join(" ")
+                    )
+                }
+            }
+            ExprKind::Then(e1, e2) => write!(f, "(({e1}); ({e2}))"),
+            ExprKind::Var(v) => write!(f, "{v}"),
+            ExprKind::App(e1, args) => write!(
+                f,
+                "({e1} {args})",
+                args = args
+                    .iter()
+                    .map(|e| format!("({e})"))
+                    .collect::<Vec<_>>()
+                    .join(" ")
+            ),
+            ExprKind::Tuple(es) => write!(
+                f,
+                "({})",
+                es.iter()
+                    .map(|e| format!("({e})"))
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            ),
+            ExprKind::ArrayMake(e1, e2) => write!(f, "Array.make ({e1}) ({e2})"),
+            ExprKind::Get(e1, e2) => write!(f, "{e1}.({e2})"),
+            ExprKind::Set(e1, e2) => write!(f, "{e1} <- ({e2})"),
         }
     }
 }
